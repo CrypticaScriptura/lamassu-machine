e3911045f43578fdbd8fe52944db073aa7fb2008
CrypticaScriptura <7396812+CrypticaScriptura@users.noreply.github.com>
2025-03-05 14:46:08 -0500

    feat: add initial centurion support

---
+++ hardware/system/centurion/cryptoatm-nonrc/calibrate-screen.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env sh
+set -e
+
+### CHANGE CONFIGURATION AT THE END OF THE FILE ###
+
+configure_screen() {
+	local rotation="$1"
+	case "${rotation}" in
+		normal) touchRotation='1 0 0 0 1 0 0 0 1';;
+		left) touchRotation='0 -1 1 1 0 0 0 0 1';;
+		right) touchRotation='0 1 0 -1 0 1 0 0 1';;
+		*) echo "Unknown rotation '${rotation}'..."; exit 1;;
+	esac
+	screenRotation="${rotation}"
+
+	xrandr -o "${screenRotation}" \
+		&& xset s off \
+		&& xset s noblank \
+		&& xset -dpms
+}
+
+configure_touch() {
+	local screen="$1"
+	xinput set-prop "${screen}" --type=float 'Coordinate Transformation Matrix' $touchRotation
+}
+
+### CHANGE CONFIGURATION BELOW TO MATCH THE CONNECTED SCREEN ###
+
+# One of: normal, left, right
+configure_screen left
+# Find the screen name under "Virtual core pointer" of the following command:
+#   xinput list
+configure_touch 'Silicon Works Multi-touch Device'
+configure_touch 'Silicon Works Multi-touch SW4101C'
